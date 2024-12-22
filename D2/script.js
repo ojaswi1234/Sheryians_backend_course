69148345c3f5cf27ce8d8a544a1b31527569ce2e
Ojaswi <ojaswideep2020@gmail.com>
2024-12-22 09:05:31 +0530

    D2 in Progress

---
+++ D2/script.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+// File CREATION
+/*
+fs.writeFile("hey.txt", "Hello World", (err) =>{
+    if(err) console.error(err);
+    else console.log("File created");
+});
+*/
+
+//APPEND
+/*
+fs.appendFile("hey.txt", "Hello World", (err) =>{
+    if(err) console.error(err);
+    else console.log("File created");
+});
+*/
+
+//RENAME
+/*
+fs.rename("hey.txt", "hello.txt", (err) =>{
+    if(err) console.error(err);
+    else console.log("File renamed");
+});
+*/
