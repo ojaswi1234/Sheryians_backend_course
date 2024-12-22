0872183a6dfdb31f41747379326770917af32823
Ojaswi <ojaswideep2020@gmail.com>
2024-12-23 05:06:34 +0530

    Middleware lesson done

--- D3/script.js
+++ D3/script.js
@@ -3,12 +3,25 @@ const app = express()
 
 // app.get(route, requestHandler)  In Case of Express, requestHandler is a MiddleWare Function
 
+app.use((req, res, next) =>{
+    console.log("MiddleWare Function first time");
+    next();
+});
+
+app.use((req, res, next) =>{
+    console.log("MiddleWare Function second time");
+    next();
+});
+
 app.get("/",(req, res) =>{
     res.send("Champion mera bhai");
-})
+});
 
-app.get("/profile",(req, res) =>{
-    res.send("Champion uska coash");
+app.get("/profile",(req, res, next) =>{
+    return next(new  Error("Something went wrong"));
+});
+app.use((err, req, res, next) => { //PUT THIS IN THE END OF THE CODE
+    console.error(err.stack);
+    res.status(500).send("Error Occured");
 })
-
-app.listen(3000);
+app.listen(3001);
