4e255fbdadf08a61c5ea8c7f72d9015fb149ebd5
Ojaswi <ojaswideep2020@gmail.com>
2025-01-03 05:40:19 +0530

    Mongo DB Essentials done

--- D6/app.js
+++ D6/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./model/user');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -8,5 +9,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
+
+app.get("/read", async (req, res) => {
+    let users = await userModel.find();
+    res.render("read", { users });
+});
+
+app.post("/create", async (req, res) =>{
+    let{name, email, imageUrl} = req.body;
+
+    let user = await userModel.create({
+        name,
+        email,
+        imageUrl
+    });
+    res.redirect('/read');
+    });
+
+app.get("/delete/:id", async (req, res) => {
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect('/read');
+});
+app.get("/edit/:id", async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.id});
+    res.render('edit', {user});
+});
+app.post("/update/:id", async (req, res) => {
+    let {name, email, imageUrl} = req.body;
+    let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, imageUrl}, {new: true});
+    res.redirect('/read');
+});
 app.listen(3000);
--- D6/model/user.js
+++ D6/model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/firstdb', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017/firstdb');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -15,3 +15,4 @@ const userSchema = mongoose.Schema({
         required: true,
     }
 });
+module.exports =  mongoose.model('user', userSchema);
